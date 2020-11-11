7240fc031f7f4e46ec6e5511eee8c554e780d2bd
isaccanedo <isaccanedo@gmail.com>
2020-11-11 15:53:29 -0300

    Initial commit

---
+++ src/main/java/br/com/isaccanedo/SmsPojo.java
@@ -0,0 +1,22 @@
+package br.com.isaccanedo;
+
+public class SmsPojo {
+
+	private String to;
+    private String message;
+    
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+    
+    
+}
